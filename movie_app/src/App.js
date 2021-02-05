d111a539f073ab8421f23fa7ebd9a908ec90425c
imnotmoon <imnotmoon@gmail.com>
2021-02-06 00:47:39 +0900

    react jsx : html+javascript. props를 통한 data의 이동

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
+function Food(props) {
+  // props를 받아서 component-component 데이터 전달
+  // father to children
+  console.log(props.favourite)      // web console에 찍힘
+  let favourite = props.favourite;
+  return(<h1> I like {favourite} </h1>)
+}
+
+// component : HTML을 반환하는 함수
 function App() {
   return (
     <div className="App">
-      Hello world
+      <h1>Hello world</h1>
+      <Food favourite="sushi" />
+      <Food favourite="ramen" />
+      <Food favourite="staek" />
     </div>
   );
 }
 
 export default App;
+
+// jsx : html + javascript
--- movie_app/src/index.js
+++ movie_app/src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />, document.getElementById('root')
 );
+
+// JSX : 자바스크립트와 HTML의 조합. 
+// 리액트는 component를 html처럼 사용하기 위해 필요.
+// 리액트에 국한된 개념.
+
+// react application은 한번에 하나의 component만 rendering 할 수 있음.
+// ReactDOM.render(<App /><Potato />...) 이런건 안됨
