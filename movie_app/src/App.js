f68b9b60129b51c117fab16f100a692f26e8f991
imnotmoon <imnotmoon@gmail.com>
2021-02-06 02:37:16 +0900

    propTypes를 통한 prop 체크, loop를 사용할 때 key prop을 설정해줘야 함

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 function Food(props) {
   // props를 받아서 component-component 데이터 전달
@@ -21,6 +22,14 @@ const foods = [
 ]
 // 각각은 Object임을 기억하자
 
+
+// 우리가 원하는 prop들이 제대로 전달되고있는지 체크해주는 모듈
+// 타입까지 체크해줌. 굉장히 유용하다. 필요한 prop type인지도 체크.
+foods.PropTypes = {
+  name : PropTypes.string.isRequired
+}
+
+
 // component : HTML을 반환하는 함수
 function App() {
   return (
@@ -38,3 +47,8 @@ export default App;
 
 // **** map : 각 array의 원소에 대해서 function을 수행하고 결과를 담은 array를 리턴.
 // array.map(cb) : cb의 리턴값을 담은 배열을 리턴
+
+//! key props가 없다는 warning
+// 각각의 component들은 unique해야하는데 루프를 돌면 uniqueness가 사라짐
+// key=""를 설정해줘야한다.
+// 보통은 id로 설정
