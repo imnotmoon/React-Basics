c52852631c52d68ed534767e6a8ef89b597083ef
imnotmoon <imnotmoon@gmail.com>
2021-02-06 19:40:16 +0900

    applying css

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -30,7 +30,7 @@ export default class App extends Component {
         : (
           <div className="movies">
             {movies.map(movie => (
-              <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+              <Movie key={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres} />
             ))}
             </div>
         )}
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -3,23 +3,25 @@ import PropTypes from 'prop-types'
 import "./Movie.css"
 
 // state가 필요 없어서 function으로 사용
-export default function Movie({year, title, summary, poster}) {
+export default function Movie({year, title, summary, poster, genres}) {
     return (
-        <div className ="movies__movie">
+        <div className ="movie">
             <img src={poster} alt={title} title={title}></img>
             <div className="movie_data">
                 <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
-                <p className="movie__summary">{summary}</p>
+                <p className="movie__summary">{summary.slice(0, 140)}...</p>
+                <ul className="movie__genres">{genres.map((genre, index) => <li key={index} className="genres_genre">{genre}</li>)}</ul>
             </div>
         </div>
     )
 }
 
 Movie.propTypes = {
-    id : PropTypes.number.isRequired,
+    // id : PropTypes.number.isRequired,
     year : PropTypes.number.isRequired,
     title : PropTypes.string.isRequired,
     summary : PropTypes.string.isRequired,
-    poster : PropTypes.string.isRequired,    
+    poster : PropTypes.string.isRequired,
+    genres : PropTypes.arrayOf(PropTypes.string).isRequired
 }
