aae0e94cefa6261bbcec82dff1b54b400e62c959
imnotmoon <imnotmoon@gmail.com>
2021-02-11 13:36:20 +0900

    state : 내부적으로 사용되는 변수들. 하위 component로 주입이 가능하다. + key props

--- egoing/my-react/src/App.js
+++ egoing/my-react/src/App.js
@@ -5,12 +5,34 @@ import Subject from './components/Subject'
 
 
 export default class App extends Component {
+  
+  // 어떤 컴포넌트가 실행될 때 render보다 먼저 실행돼서 초기화를 담당
+  constructor(props) {
+    super(props);
+
+    // 외부에서 알 필요가 없는 코드를 숨김. 내부적으로 사용
+    // Subject라고 하는 태그에 props값으로 준 것.
+    // 상위 컴포넌트인 App의 상태를 하위 컴포넌트로 전달하고 싶을 때 사용
+    //! App.state -> Subject.props
+    this.state = {
+      subject: {title : 'Web', sub: 'World Wide Web'},
+      
+      // 여러개의 엘리먼트를 생성할때는 유니크한 key가 필요함.
+      // 하위 컴포넌트인 TOC에 전달할 state. -> props
+      contents: [
+        {id : 1, title : "HTML", desc : "HTML is Hyper Text..."},
+        {id : 2, title : "CSS", desc : "CSS is for design"},
+        {id : 3, title : "Javascript", desc : "Javascript is for interactive"},
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"/>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}/>
         <Subject title="React" sub="For UI"/>
-        <TOC />
+        <TOC data={this.state.contents}/>
         <Content title="HTML" desc="HyperText Markup Language."/>
       </div>
     )
--- egoing/my-react/src/components/TOC.js
+++ egoing/my-react/src/components/TOC.js
@@ -2,12 +2,19 @@ import React, {Component} from 'react';
 
 export default class TOC extends Component {
     render() {
+        var lists = [];
+        var data = this.props.data
+        var i=0;
+        
+        // id는 React가 내부적으로 필요로 하는 prop이다.
+        while(i<data.length) {
+            lists.push(<li key={data[i].id}><a href={"/contents/"+data[i].id}>{data[i].title}</a></li>)
+            i += 1;
+        }
         return (
         <nav>
             <ul>
-                <li><a href="1.html">HTML</a></li>
-                <li><a href="2.html">CSS</a></li>
-                <li><a href="3.html">Javascript</a></li>
+                {lists}
             </ul>
         </nav>
         )
