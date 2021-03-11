407a58e93c061f8f033c7b58cf3b46f94bd4d0c9
imnotmoon <imnotmoon@gmail.com>
2021-03-12 06:13:32 +0900

    todo-list with state

---
+++ ReactProgrammingPractical/chapter02/todo-list/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import React, { useState } from 'react'
+
+function App() {
+
+  const [desc, setDesc] = useState("");
+  const [currentId, setCurrentId] = useState(1);
+  const [todoList, setTodoList] = useState([]);
+
+  function onAdd() {
+    const todo = { id: currentId, desc }
+    setTodoList([...todoList, todo])
+    setCurrentId(currentId+1)
+  }
+
+  function onDelete(e) {
+    const id = Number(e.target.dataset.id);
+    const newTodoList = todoList.filter(todo => todo.id !== id)
+    setTodoList(newTodoList)
+  }
+
+  function onSaveToServer() {
+    // logic to save server
+  }
+
+  return (
+    <div>
+      <h3>할 일 목록</h3>
+      <ul>
+        {todoList.map(todo => (
+          <li key={todo.id}>
+            <span>{todo.desc}</span>
+            <button data-id={todo.id} onClick={onDelete}>삭제</button>
+          </li>
+        ))}
+      </ul>
+      <input type="text" value={desc} onChange={e => setDesc(e.target.value)} />
+      <button onClick={onAdd}>추가</button>
+      <button onClick={onSaveToServer}>서버에 저장</button>
+    </div>
+  );
+}
+
+export default App;
