3ee304d334bde61de06f51b8d6ea296f931127e7
imnotmoon <imnotmoon@gmail.com>
2021-03-29 05:23:50 +0900

    todo-list

---
+++ ReactProgrammingPractical/chapter03/todo-list/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import List from './List'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo-list</h1>
+      <List />
+    </div>
+  );
+}
+
+export default App;
