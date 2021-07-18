68f403a8f6c3fbfed702ae7511a81b315763212d
imnotmoon <imnotmoon@gmail.com>
2021-07-19 00:13:43 +0900

    week1 - 함수형 컴포넌트

--- SpartaCodingClub/sparta_react/week-1/src/App.js
+++ SpartaCodingClub/sparta_react/week-1/src/App.js
@@ -1,24 +1,15 @@
+import React from "react";
 import "./App.css";
+// BucketList 컴포넌트를 import 해옵니다.
+// import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
+import BucketList from "./BucketList";
 
 function App() {
-	const styles = {
-		border: "1px solid #eee",
-		padding: "20px",
-		display: "flex",
-		width: "100vw",
-		maxWidth: "400px",
-		margin: "30px auto",
-		flexDirection: "column",
-	};
-
 	return (
 		<div className="App">
-			<div style={styles}>
-				<h1 style={{ color: "green" }}>안녕하세요!</h1>
-				<hr style={{ width: "100%" }} />
-				<p style={{ textAlign: "left" }}>이름을 입력해주세요.</p>
-				<input type="text" />
-			</div>
+			<h1>내 버킷리스트</h1>
+			{/* 컴포넌트를 넣어줍니다. */}
+			<BucketList />
 		</div>
 	);
 }
---
+++ SpartaCodingClub/sparta_react/week-1/src/BucketList.js
@@ -0,0 +1,22 @@
+// 리액트 패키지를 불러옵니다.
+import React from "react";
+
+// 함수형 컴포넌트는 이렇게 쓸 수도 있고
+// function Bucketlist(props){
+//     return (
+//         <div>버킷 리스트</div>
+//     );
+// }
+
+// 이렇게 쓸 수도 있어요. =>가 들어간 함수를 화살표 함수라고 불러요.
+// 저희는 앞으로 화살표 함수를 사용할거예요.
+// 앗 () 안에 props! 부모 컴포넌트에게 받아온 데이터입니다.
+// js 함수가 값을 받아오는 것과 똑같이 받아오네요.
+const BucketList = (props) => {
+	// 컴포넌트가 뿌려줄 ui 요소(리엑트 엘리먼트라고 불러요.)를 반환해줍니다.
+	return <div>버킷 리스트</div>;
+};
+
+// 우리가 만든 함수형 컴포넌트를 export 해줍니다.
+// export 해주면 다른 컴포넌트에서 BucketList 컴포넌트를 불러다 쓸 수 있어요.
+export default BucketList;
