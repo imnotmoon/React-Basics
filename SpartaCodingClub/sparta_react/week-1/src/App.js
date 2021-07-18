a171d0d56ba6c54b5ce77a94354885882b108347
imnotmoon <imnotmoon@gmail.com>
2021-07-19 00:30:12 +0900

    week1 - css

--- SpartaCodingClub/sparta_react/week-1/src/App.js
+++ SpartaCodingClub/sparta_react/week-1/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import "./App.css";
 // BucketList 컴포넌트를 import 해옵니다.
 // import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
 import BucketList from "./BucketList";
+import "./style.css";
 
 // 클래스형 컴포넌트는 이렇게 생겼습니다!
 class App extends React.Component {
@@ -16,11 +16,18 @@ class App extends React.Component {
 
 	// 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
 	render() {
+		// this 키워드를 통해 state에 접근할 수 있어요.
+		console.log(this.state);
+
 		return (
 			<div className="App">
-				<h1>내 버킷리스트</h1>
-				{/* 컴포넌트를 넣어줍니다. */}
-				<BucketList list={this.state.list} />
+				<div className="container">
+					<h1 className="title">내 버킷리스트</h1>
+					<hr className="line" />
+					{/* 컴포넌트를 넣어줍니다. */}
+					{/* <컴포넌트 명 [props 명]={넘겨줄 것(리스트, 문자열, 숫자, ...)}/> */}
+					<BucketList list={this.state.list} />
+				</div>
 			</div>
 		);
 	}
